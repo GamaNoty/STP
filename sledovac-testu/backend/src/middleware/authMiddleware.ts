40bb5d5346a0f03815458449a2dd33a0652ced9c
GamaNoty <noga.ma.2023@skola.ssps.cz>
2026-01-18 14:12:57 +0100

    Implement JWTs

---
+++ sledovac-testu/backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,17 @@
+import { type Request, type Response, type NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) return res.status(401).json({ message: 'Přístup odepřen' });
+
+  jwt.verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
+    if (err) return res.status(403).json({ message: 'Neplatný token' });
+    
+    // Přidáme data uživatele do objektu požadavku pro další použití
+    (req as any).user = user;
+    next();
+  });
+};
