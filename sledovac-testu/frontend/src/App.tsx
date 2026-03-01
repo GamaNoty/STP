7178e58f0f4b8dd35b92d1565ed9f6eeeb021194
GamaNoty <noga.ma.2023@skola.ssps.cz>
2026-03-01 14:43:12 +0100

    import sidebar

--- sledovac-testu/frontend/src/App.tsx
+++ sledovac-testu/frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import { Sidebar } from './components/Sidebar';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="flex min-h-screen bg-brand-bg text-brand-text font-sans">
+      <Sidebar />
+
+      <main className="flex-1 p-8">
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
