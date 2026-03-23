e4a18cd30d895b688ba9565a13fa2d9b23e2d59d
GamaNoty <noga.ma.2023@skola.ssps.cz>
2026-03-23 13:01:02 +0100

    Fix topnav buttons and functionality

--- sledovac-testu/frontend/src/components/TopNav.tsx
+++ sledovac-testu/frontend/src/components/TopNav.tsx
@@ -1,9 +1,13 @@
-import { Bell, Settings, User } from "lucide-react";
+import { /*Bell, Settings,*/ User, LogOut } from "lucide-react";
 import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export const TopNav = () => {
   const location = useLocation();
+  
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+  };
 
   const pageNames: Record<string, string> = {
     "/": "Dashboard",
@@ -19,31 +23,42 @@ export const TopNav = () => {
   useEffect(() => {
     document.title = `${currentPageName} | Sledovač testů`;
   }, [currentPageName]);
+  const isAuthenticated = !!localStorage.getItem('token');
 
   return (
     <header className="flex justify-between items-center mb-8">
       <div>
         <div className="text-brand-textMuted text-xs mb-1 flex gap-1">
-          <span>Pages</span>
+          <span>Stránky</span>
           <span>/</span>
           <span className="text-white">{currentPageName}</span>
         </div>
       </div>
 
       <div className="flex items-center gap-4 text-brand-textMuted">
-        <Link
-          to="/signin"
-          className="bg-white/5 hover:bg-white/10 text-white px-6 py-2 rounded-full font-bold transition-colors border border-white/5 inline-flex items-center justify-center"
-        >
-          <User size={16} />
-          <span>Sign In</span>
-        </Link>
-        <button className="hover:text-white transition-colors">
+      {isAuthenticated ? (
+          <Link to={'/signin'}
+            className="flex items-center gap-2 hover:text-brand-red transition-colors text-sm font-medium"
+            onClick={handleLogout}
+          >
+            <LogOut size={16} />
+            <span>Odhlásit se</span>
+          </Link>
+        ) : (
+          <Link to={'/signin'}
+            className="flex items-center gap-2 hover:text-white transition-colors text-sm font-medium"
+          >
+            <User size={16} />
+            <span>Přihlásit se</span>
+          </Link>
+        )}
+        {/*<button className="hover:text-white transition-colors">
           <Settings size={16} />
         </button>
         <button className="hover:text-white transition-colors">
           <Bell size={16} />
         </button>
+        */}
       </div>
     </header>
   );
